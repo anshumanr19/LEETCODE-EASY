d05c40840e6e6d82fd207a30c547da4bd721fc60
Anshuman Rout <anshumanr445@gmail.com>
2025-06-04 20:02:37 +0530

    Remove Duplicates from Sorted List added

---
+++ com/leetcode_easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,44 @@
+package com.leetcode_easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        head = deleteDuplicates(head);
+
+        printLinkedList(head);
+    }
+
+    public static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
